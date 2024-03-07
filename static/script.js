ca2013a8f987bb9b79b71425a2417ad5ba0423a2
Rayhansatria733 <160561074+Rayhansatria733@users.noreply.github.com>
2024-03-07 09:46:00 +0000

    initial commit

--- static/script.js
+++ static/script.js
@@ -80,7 +80,7 @@ const populateBotResponse = async (userMessage) => {
   let uploadButtonHtml = '';
 
   if (isFirstMessage) {
-    response = { botResponse: "Hello there! I'm your friendly data assistant, ready to answer any questions regarding your data. Could you please upload a PDF file for me to analyze?"};
+    response = { botResponse: "Hello You! I'm your friendly PDF Analyzer hehe.., ready to answer any questions regarding your data. Could you please upload a PDF file for me to analyze? hehehe.."};
     uploadButtonHtml = `
         <input type="file" id="file-upload" accept=".pdf" hidden>
         <button id="upload-button" class="btn btn-primary btn-sm">Upload File</button>
